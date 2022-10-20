342b8618aa465afcdea5aa129751a9ff9cc65b67
JJ1104 <jithinjose1104@gmail.com>
2022-10-20 11:13:57 +0530

    W8L8

--- Lab8/Q5/qb.v
+++ Lab8/Q5/qb.v
@@ -1,11 +1,13 @@
 module encoder16to4(a,f);
 	input [15:0] a;
-	output reg [4:0] f;
+	output reg [3:0] f;
 	integer i;
 	always @(a)
 	begin
 		for(i=0;i<16;i=i+1)
 		begin
-			
+			if(a[i] == 1)
+				f=i;
+		end
 	end
 endmodule
